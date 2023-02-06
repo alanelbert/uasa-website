2086bc3528493a9b1ca4d6b3b7e9ac681ebcde65
Alan Elbert <aelbert@terpmail.umd.edu>
2023-02-06 12:38:34 -0500

    Fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
   Link
