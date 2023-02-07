75c9f58f50e3bff45ecb0ae8490e89be79b53e5e
Alan Elbert <aelbert@terpmail.umd.edu>
2023-02-06 22:10:59 -0500

    Adding about section (Temp)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 
-
+import './About.css'
 
 
 
 function About() {
     return (
-        <div>
+        <div className="about-card">
             <h1>About UASA</h1>
+
+            <p>The Ukrainian Student Association is established for the expressed purpose of providing 
+                a space for and supporting students of Ukrainian descent. We hold fundraisers and conduct social activism in support of the people in Ukraine. Members have the opportunity to meet other students that have a connection to Ukrainian culture and participate in activism on behalf of Ukraine. The organization is open to anyone expressing interest in Ukrainian culture and moves to educate the surrounding University of Maryland community on our heritage.</p>
+        
+            <p>
+                The Ukrainian Student Association (UASA) strives to facilitate a supportive community for students of Ukrainian descent and to promote Ukraine’s culture within the University of Maryland community.
+                 Our purpose is to educate our community on Ukrainian culture, history, language, and customs as well as support people affected by the ongoing war in Ukraine. We hold fundraisers and conduct social activism in support of the people in Ukraine. The organization is open to anyone within the UMD community expressing interest in Ukrainian culture and seeking ways to support Ukraine and its people.
+
+
+            </p>
+
+            <p>
+            In less than a year, the members of the Ukrainian Student Association participated in dozens rallies and protests in Washington DC. The members off UASA organized the Candlelight Vigil to commemorate the victims of the war in Ukraine at the McKeldin steps. 
+            UASA conducted several blue & yellow ribbon-making event along with the sunflower crocheting workshop and their distribution to raise awareness about the war Russia started in Ukraine at the UMD campus. The UASA members  packed the medical equipment and humanitarian aid that was shipped to Ukraine and found numerous suitcases for their transportation. We organized numerous fundraising events such as the Ukrainian movie screening fundraising event, participated in the First International Student Festival, Maryland Day 2022 and the Fist Look Fair 2022 and raised more than $1,200 for humanitarian aid and purchase of the new ambulances in Ukraine. The UASA members gave numerous interviews  and participated in the library lectures, student panels, and the Ukrainian Summit at Capitol Hill to advocate for Ukraine. 
+            </p>
+
+            <p>
+            We are a new organization, but we are very united, we are expanding, and we constantly work on the ways how we can support each other and the people of Ukraine in the Russian war. Our ultimate goal is to speed up the end of the war, but we are planning to remain together as an organization as a to further promote Ukrainian culture, language and traditions as well as support the incoming international students from Ukraine.
+            </p>
+        
+        
         </div>
     );
 
