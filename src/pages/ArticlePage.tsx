fd1c12e677adbe9e626a621d23fff49e954bda13
Alan Elbert <aelbert@terpmail.umd.edu>
2024-04-14 04:34:54 -0400

    fixing

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -11,11 +11,11 @@ interface ArticleContent {
     timeStamp: string;
     message: string;
     title: string;
-
+    image : string;
 }
 
 
-function ArtPost ({title, author, timeStamp, message} : ArticleContent) {
+function ArtPost ({title, author, timeStamp, message, image} : ArticleContent) {
     return (
         
         <div className="">
@@ -23,8 +23,16 @@ function ArtPost ({title, author, timeStamp, message} : ArticleContent) {
             <div className="">
                 <div className="">
                     <h1 className="">{title}</h1>
-                    <h2 className="">Author: {author}</h2>
                     <p className="">{(new Date(parseFloat(timeStamp))).toDateString()}</p>
+                    {
+                    author != "" &&
+                    <h2 className="">Author: {author}</h2>
+                    }
+                    {
+                        image != "" &&
+                        <img src={image}></img>
+                    }
+                    
                 </div>
                 <p>{message}</p>
             </div>
@@ -37,7 +45,7 @@ function ArtPost ({title, author, timeStamp, message} : ArticleContent) {
 function ArticleContent() {
 
     const aId = useParams().id;
-    const [article , setArticle] = useState<ArticleContent>({ 'author': '', 'title' : '', 'message' : '', 'timeStamp' : '0'});
+    const [article , setArticle] = useState<ArticleContent>({ 'author': '', 'title' : '', 'message' : '', 'timeStamp' : '0', 'image':""});
 
     useEffect(() => {
         const url : string = "http://localhost:9000/v1/blogs/" + aId;
@@ -51,12 +59,27 @@ function ArticleContent() {
         })
     }, [])
 
+    if (aId == "vigil-article") {
+        return (
+            <div>
+            <ArtPost message={'Join the Candlelight Vigil for Ukraine at McKeldin Mall near the sundial from 4 to 5 pm on Thursday, February 23rd to honor the victims of the war in Ukraine. This day will mark a year of a full-scale war in Ukraine. This war has resulted in significant losses of human life and millions of displaced people as well as endangered democracy in Europe and the entire world.\nWith this event, the Ukrainian Student Association would like to honor the victims of the war and share how we feel now, a year after. We would also like to unite and thank the UMD community for the continuing support of Ukraine, the Ukrainian people, and Ukrainian UMD students.\nSadly, the ongoing war continues to bring suffering to millions of people every day. We, therefore, encourage the UMD community to continue taking action and reach out to UASA about the ways you can help.\nPlease, help spread the word about the event. You can bring flowers and/or donate to UASA to help the people of Ukraine.'} 
+                        author={""}
+                        timeStamp={"1676021954454"}
+                        title={"UASA Candlelight Vigil"}
+                        image={"/imageflyer.jpeg"}
+                         /> 
+            </div>
+        )
+        
+    }
+
     return (
     <div>
          <ArtPost author={article.author} 
                         message={article.message}
                         timeStamp={article.timeStamp}
                         title={article.title}
+                        image={""}
                          /> 
     </div>);
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,14 @@ import "./Home.css"
 interface ArticleContent {
     author: string;
     timeStamp: string;
+    img : string;
     message: string;
     title: string;
     id: string;
 }
 
 
-function BlogPost ({title, author, timeStamp, message, id} : ArticleContent) {
+function BlogPost ({title, author, timeStamp, img, message, id} : ArticleContent) {
     return (
         
         <div className="article-card">
@@ -20,7 +21,15 @@ function BlogPost ({title, author, timeStamp, message, id} : ArticleContent) {
                 <div className="article-card-content">
                     <div className=" article-info">
                         <h1 className="article-title article-subhead">{title}</h1>
-                        <h2 className="article-author article-subhead">Author: {author}</h2>
+                        {
+                            img != "" &&
+                            <img src={img} className="article-image"></img>
+                        }
+
+                        {
+                            author != "" &&
+                            <h2 className="article-author article-subhead">Author: {author}</h2>
+                        }
                         <p className="article-date article-subhead">{(new Date(parseFloat(timeStamp))).toDateString()}</p>
                     </div>
                     <p>{message.length < 500 ? message : message.substring(0, 500) + '...'}</p>
@@ -59,13 +68,25 @@ function Home() {
             </div>
             <br></br>
             <div className="article-area">
+
+                <BlogPost author={""}
+                                message = {'Join the Candlelight Vigil for Ukraine at McKeldin Mall near the sundial from 4 to 5 pm on Thursday, February 23rd to honor the victims of the war in Ukraine. This day will mark a year of a full-scale war in Ukraine. This war has resulted in significant losses of human life and millions of displaced people as well as endangered democracy in Europe and the entire world.\nWith this event, the Ukrainian Student Association would like to honor the victims of the war and share how we feel now, a year after. We would also like to unite and thank the UMD community for the continuing support of Ukraine, the Ukrainian people, and Ukrainian UMD students.\nSadly, the ongoing war continues to bring suffering to millions of people every day. We, therefore, encourage the UMD community to continue taking action and reach out to UASA about the ways you can help.\nPlease, help spread the word about the event. You can bring flowers and/or donate to UASA to help the people of Ukraine.'}
+                                img = '/vigil.jpeg'
+                                timeStamp="1676021954454"
+                                id = {"vigil-article"}
+                                title={"UASA Vigil"}
+
+                            />
                 {
+
+                    
                     articles.map(article => 
                         (
                             <BlogPost author={article.author} 
                                         message={article.message}
                                         timeStamp={article.timeStamp}
                                         title={article.title}
+                                        img = {""}
                                         id = {article._id} /> 
                         )
                         )
