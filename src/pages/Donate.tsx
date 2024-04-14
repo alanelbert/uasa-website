c412d31c2acdf8446c3cd9f0c627fc838ec2e012
Alan Elbert <aelbert@terpmail.umd.edu>
2024-04-14 12:33:37 -0400

    yes

---
+++ src/pages/Donate.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './Donate.css'
+
+
+
+function Donate() {
+    return (
+        <div className="donate-card">
+            <p>yes</p>
+        
+        </div>
+    );
+
+}
+
+
+export default Donate;
